af3a5d012e4856ed16348705cc3fda0455563139
iamShah <formal>
2020-11-12 20:38:49 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,61 +1,44 @@
-
-def sort_array_asc(arr)
-  arr.sort
+def sort_array_asc(array)
+  array.sort
 end
 
-def sort_array_desc(arr)
-  arr.sort do |x,y|
-    y<=>x
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b| a <=> b }
+end
+
+def swap_elements(array)
+  switch = array.pop
+  array.insert(1, switch)
+end
+
+def swap_elements_from_to(array, from, to)
+  array[from], array[to] = array[to], array[from]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(names)
+  names.map { |name| name[2] = "$" } 
+  names
+end
+
+def find_a(array)
+  array.select { |item| item[0] == "a" }
+end
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def add_s(array)
+  array.map.with_index do |item, index|
+    index != 1 ? item << "s" : item
   end
-end
-# Question 3
-#   describe '#sort_array_char_count' do
-#     it 'should return an array in ascending order sorted by the number of characters in the string' do
-#       expect(sort_array_char_count(["dogs", "cat", "Horses"])).to eq(["cat", "dogs", "Horses"])
-#     end
-#   end
- def sort_array_char_count(arr)
- arr.sort{|x, y| x.length <=> y.length}
-end
-
-
- should return an array in ascending order sorted by the number of characters in the string (FAILED - 1)
-def swap_elements(arr)
-  arr[2], arr[1] = arr[1], arr[2]
-  arr
-end
-
-def swap_elements_from_to(arr,x,y)
-  arr[x], arr[y] = arr[y], arr[x]
-  arr
-end
-
-def reverse_array(arr)
-  arr.reverse
-end
-
-def kesha_maker(arr)
-  for i in 0...arr.length
-    arr[i][2] = "$"
-  end
-  arr
-end
-
-def find_a(arr)
-  arr.select do |x|
-    x[0] == "a"
-  end
-end
-
-def sum_array(arr)
-  arr.inject(:+)
-end
-
-def add_s(arr)
-  for i in 0...arr.length
-    if i != 1
-      arr[i] += "s"
-    end
-  end
-  arr
-end
+end 
